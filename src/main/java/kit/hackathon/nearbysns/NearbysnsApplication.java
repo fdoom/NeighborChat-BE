ca4a34100e1fcf3861f8576d77ef1c9180580681
Onji Kim <xodks672@gmail.com>
2024-06-22 22:32:02 +0900

    init : base structure

---
+++ src/main/java/kit/hackathon/nearbysns/NearbysnsApplication.java
@@ -0,0 +1,13 @@
+package kit.hackathon.nearbysns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbysnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbysnsApplication.class, args);
+	}
+
+}
