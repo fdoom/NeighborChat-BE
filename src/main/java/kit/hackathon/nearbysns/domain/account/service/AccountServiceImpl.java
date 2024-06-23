1e7205c0c48a11b273f762ed01548ac75d7f3978
fdoom <11jjklol@naver.com>
2024-06-23 23:05:20 +0900

    세션 정보 기반 계정 고유키 반환 기능 추가, 예제 추가(TestController)

--- src/main/java/kit/hackathon/nearbysns/domain/account/service/AccountServiceImpl.java
+++ src/main/java/kit/hackathon/nearbysns/domain/account/service/AccountServiceImpl.java
@@ -13,15 +13,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -60,12 +56,8 @@ public class AccountServiceImpl implements AccountService {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         httpSession.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
-
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return AccountLoginResponseDTO.builder()
                 .sessionId(httpSession.getId())
-                .accountRole(userDetails.getAuthorities().toString())
-                .accountLoginId(userDetails.getUsername())
                 .build();
     }
 }
