643b229a044f2ccadc636266b3d83fed37e03c1b
fdoom <11jjklol@naver.com>
2024-06-24 09:56:56 +0900

    요청 검사 및 로그인 시 응답코드만 반환

--- src/main/java/kit/hackathon/nearbysns/domain/account/dto/request/AccountRegisterRequestDTO.java
+++ src/main/java/kit/hackathon/nearbysns/domain/account/dto/request/AccountRegisterRequestDTO.java
@@ -1,13 +1,19 @@
 package kit.hackathon.nearbysns.domain.account.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.ToString;
 
 @ToString
 @Getter
 public class AccountRegisterRequestDTO {
+    @NotBlank(message = "사용자 계정에 대한 이름은 빈칸이 아니여야 합니다.")
     private String accountName;
+
+    @NotBlank(message = "사용자 계정에 대한 로그인 아이디는 빈칸이 아니여야 합니다.")
     private String accountLoginId;
+
+    @NotBlank(message = "사용자 계정에 대한 로그인 비밀번호는 빈칸이 아니여야 합니다.")
     private String accountLoginPw;
 
     public void updatePassword(String accountLoginPw) {
