7dfccb797ea326a8323a71024dfe2f8c7824ebc0
fdoom <11jjklol@naver.com>
2024-06-24 12:42:48 +0900

    에러 코드 메시지 수정

--- src/main/java/kit/hackathon/nearbysns/domain/account/dto/request/AccountLoginRequestDTO.java
+++ src/main/java/kit/hackathon/nearbysns/domain/account/dto/request/AccountLoginRequestDTO.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 public class AccountLoginRequestDTO {
     @NotBlank(message = "사용자 계정에 대한 로그인 아이디는 빈칸이 아니여야 합니다.")
     private String accountLoginId;
+
     @NotBlank(message = "사용자 계정에 대한 로그인 비밀번호는 빈칸이 아니여야 합니다.")
     private String accountLoginPw;
 }
