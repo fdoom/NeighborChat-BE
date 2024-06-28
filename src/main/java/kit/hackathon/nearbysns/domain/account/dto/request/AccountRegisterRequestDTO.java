aa5a9170b622ab395b4049a5f2fba4f532303161
fdoom <11jjklol@naver.com>
2024-06-28 13:29:09 +0900

    fix: 회원가입 및 비밀 번호 변경 시 제약 조건 추가

--- src/main/java/kit/hackathon/nearbysns/domain/account/dto/request/AccountRegisterRequestDTO.java
+++ src/main/java/kit/hackathon/nearbysns/domain/account/dto/request/AccountRegisterRequestDTO.java
@@ -1,6 +1,7 @@
 package kit.hackathon.nearbysns.domain.account.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 
@@ -10,9 +11,11 @@ public class AccountRegisterRequestDTO {
     private String accountName;
 
     @NotBlank(message = "사용자 계정에 대한 로그인 아이디는 빈칸이 아니여야 합니다.")
+    @Size(min = 4, max = 20, message = "4글자 이상 20글자 이하여야 합니다.")
     private String accountLoginId;
 
     @NotBlank(message = "사용자 계정에 대한 로그인 비밀번호는 빈칸이 아니여야 합니다.")
+    @Size(min = 4, max = 20, message = "4글자 이상 20글자 이하여야 합니다.")
     private String accountLoginPw;
 
     public void updatePassword(String accountLoginPw) {
--- src/main/java/kit/hackathon/nearbysns/domain/account/dto/request/AccountUpdateLoginPasswordRequestDTO.java
+++ src/main/java/kit/hackathon/nearbysns/domain/account/dto/request/AccountUpdateLoginPasswordRequestDTO.java
@@ -1,6 +1,7 @@
 package kit.hackathon.nearbysns.domain.account.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
@@ -9,5 +10,6 @@ public class AccountUpdateLoginPasswordRequestDTO {
     private String currentPw;
 
     @NotBlank(message = "사용자 계정에 대한 변경할 비밀번호는 빈칸이 아니여야 합니다.")
+    @Size(min = 4, max = 20, message = "4글자 이상 20글자 이하여야 합니다.")
     private String newPw;
 }
